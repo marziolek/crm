ab92173315d6c428d70befadd3ea62f46f651ba4
marziolek <ziolkenz@interia.pl>
2012-02-05 00:52:06 +0100

    sth working in mailer

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,3 +1,10 @@
 class Notifier < ActionMailer::Base
-  default from: "from@example.com"
+  default :from => 'no-reply@example.com',
+    :return_path => 'system@example.com'
+
+  def notifie(recipient)
+    @account = recipient
+    mail(:to => recipient.email,
+         :bcc => ["bcc@example.com", "Order Watcher <watcher@example.com>"])
+  end
 end
