3fa8cd66b42ed6d4e7a8d27cf3a08452aebc6dcf
Dziolas <09_ziolekw@wierzba.wzks.uj.edu.pl>
2012-02-09 16:46:03 +0100

    ClientsApplication - update podejscie 2

--- db/migrate/20120209152018_habtm_clients_apllications.rb
+++ db/migrate/20120209152018_habtm_clients_apllications.rb
@@ -1,7 +1,11 @@
 class HabtmClientsApllications < ActiveRecord::Migration
   def up
+	add_column :clients_applications, :client_id, :integer
+	add_column :clinets_applications, :application_id, :integer
   end
 
   def down
+	remove_column :clients_applications, :client_id
+	remove_column :clinets_applications, :application_id
   end
 end
