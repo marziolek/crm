c74ac52da650d00a83f5d9b2737255513ef4bc87
Dziolas <09_ziolekw@wierzba.wzks.uj.edu.pl>
2012-02-09 17:33:36 +0100

    clients habtm applications

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,11 @@
 class Application < ActiveRecord::Base
   validates_presence_of :name, :description, :version                   
   validates_uniqueness_of :name, :scope => :version, :message => "has this Version already"
+		
+	has_many :clients_applications
+	has_many :clients, :through => :clients_applications
+	
+	def display_name
+		"#{name} - #{version}"
+	end
 end
